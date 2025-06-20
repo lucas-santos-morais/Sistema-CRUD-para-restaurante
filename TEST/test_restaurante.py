f8aea9bbec7f37079203195c10f70119c11c965c
Lucas Morais <126816145+lucas-santos-morais@users.noreply.github.com>
2025-06-20 00:18:53 +0000

    Ajustes do código SRC e TEST, implementado o pytest.ini para rodar os testes

--- TEST/test_restaurante.py
+++ TEST/test_restaurante.py
@@ -1,25 +1,29 @@
-import pytest
-from SRC import restaurante
+from restaurante import (
+    adicionar_restaurante,
+    buscar_restaurante_por_nome,
+    alternar_status_restaurante,
+    excluir_restaurante
+)
 
 def test_adicionar_restaurante():
-    resultado = restaurante.adicionar_restaurante('Teste', 'Japonesa')
+    resultado = adicionar_restaurante('Teste', 'Japonesa')
     assert resultado['nome'] == 'Teste'
     assert resultado['categoria'] == 'Japonesa'
     assert resultado['ativo'] is False
 
 def test_buscar_restaurante_por_nome():
-    restaurante.adicionar_restaurante('KFC', 'Fast Food')
-    r = restaurante.buscar_restaurante_por_nome('KFC')
+    adicionar_restaurante('KFC', 'Fast Food')
+    r = buscar_restaurante_por_nome('KFC')
     assert r is not None
     assert r['categoria'] == 'Fast Food'
 
 def test_alternar_status_restaurante():
-    restaurante.adicionar_restaurante('StatusTest', 'Vegana')
-    status_antes = restaurante.buscar_restaurante_por_nome('StatusTest')['ativo']
-    status_depois = restaurante.alternar_status_restaurante('StatusTest')
-    assert status_depois != status_antes  # status foi alternado
+    adicionar_restaurante('StatusTest', 'Vegana')
+    status_antes = buscar_restaurante_por_nome('StatusTest')['ativo']
+    status_depois = alternar_status_restaurante('StatusTest')
+    assert status_depois != status_antes
 
 def test_excluir_restaurante():
-    restaurante.adicionar_restaurante('DeletarTest', 'Saudável')
-    assert restaurante.excluir_restaurante('DeletarTest') is True
-    assert restaurante.buscar_restaurante_por_nome('DeletarTest') is None
+    adicionar_restaurante('DeletarTest', 'Saudável')
+    assert excluir_restaurante('DeletarTest') is True
+    assert buscar_restaurante_por_nome('DeletarTest') is None
